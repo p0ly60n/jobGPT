cd201ace3ca494d66aa614a40aa02c12dd444e85
p0ly60n <mar_weiss@icloud.com>
2023-11-24 16:40:21 +0100

    add openAI gpt feature

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import csv
 import os
 
 from src.scraper import extract_data
+from src.gpt import get_letter
 
 if __name__ == "__main__":
     # search queries
-    interest = input("Interest: ").strip()
-    location = input("Location: ").strip()
+    interest = input("Interest: ").strip().replace(" ", "+")
+    location = input("Location: ").strip().replace(" ", "+")
     radius = int(input("Radius: "))
-
-    no_of_pages = int(input("#Pages: "))
+    no_of_jobs = int(input("#Jobs: "))
 
     # Creating the Main Directory
     directory = os.getcwd() + "/"
@@ -19,18 +19,22 @@ if __name__ == "__main__":
 
 
     # creating the CSV-file
-    file_name = interest.title() + "_" + location.title() + "_Jobs.csv"
-    file_path = directory + file_name
+    csv_file_name = interest.title() + "_" + location.title() + "_Jobs.csv"
 
     # writing the labels to the CSV-file
-    with open(file_path, mode="w", encoding="utf8") as file:
-        writer = csv.writer(file, delimiter=",", lineterminator="\n")
-        writer.writerow(["TITLE", "COMPANY", "LOCATION", "LINK", "TEXT"])
+    with open(f"{directory}/output/{csv_file_name}", mode="w", encoding="utf8") as csv_file:
+        writer = csv.writer(csv_file, delimiter=",", lineterminator="\n")
+        writer.writerow(["TITLE", "COMPANY", "LOCATION", "LINK"])
 
 
     # run the scraper to extract the job data from the website
-    jobs = extract_data(interest, location, radius, no_of_pages)
+    jobs = extract_data(interest, location, radius, no_of_jobs)
 
     # writing the job data to the CSV-file
     for job in jobs:
-        job.write_to_file(file_path)
+        job.write_to_file(f"{directory}/output/{csv_file_name}")
+
+    for idx, job in enumerate(jobs):
+        with open(f"{directory}/output/job{idx}.txt", mode="w", encoding="utf8") as txt_file:
+            # get the resume
+            txt_file.write(get_letter(job))
