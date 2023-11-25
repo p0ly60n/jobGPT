27cfd194a569420cfb6410d0a7aa1b6af5a6f7a5
p0ly60n <mar_weiss@icloud.com>
2023-11-25 22:31:48 +0100

    prettify code

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def start_gpt_generation_threaded(job, job_index, personal_info, directory):
         print(f"Cover-Letter for job{job_index} ({job}) generated!")
         print(f"-> Tokens: in:{gpt_data["input_tokens"]}, out:{gpt_data["output_tokens"]}")
 
+
 if __name__ == "__main__":
     # search queries
     interest = input("Interest: ").strip().replace(" ", "+")
@@ -22,42 +23,42 @@ if __name__ == "__main__":
     no_of_jobs = int(input("#Jobs: "))
     pdf_file = input("Resume-File <.pdf> (leave blank if none available): ").strip()
 
+
     # getting the working root directory
     directory = os.getcwd() + "/"
-
-    csv_file_name = interest.title() + "_" + location.title() + "_Jobs.csv"
-
     # creating the output directory if it does not exist
     if not os.path.exists(directory + "/output"):
         os.mkdir(directory + "/output")
         print(f"Directory added at {directory + "/output"}")
 
+
     # writing the labels to the CSV-file
+    csv_file_name = interest.title() + "_" + location.title() + "_Jobs.csv"
     with open(f"{directory}/output/{csv_file_name}", mode="w", encoding="utf8") as csv_file:
         writer = csv.writer(csv_file, delimiter=",", lineterminator="\n")
         writer.writerow(["TITLE", "COMPANY", "LOCATION", "LINK"])
 
-    print("\nScraping...\n")
 
+    print("\nScraping...\n")
     # run the scraper to extract the job data from the website
     jobs = scraper.extract_data(interest, location, radius, no_of_jobs)
+
 
     # writing the job data to the CSV-file
     for job in jobs:
         job.write_to_file(f"{directory}/output/{csv_file_name}")
 
+
     # if a resume is available, extract the text from it
-
-    print("\nExtracting data of .pdf file if given...\n")
-
     personal_info = ""
     if (pdf_file != ""):
+        print(f"\nExtracting data of {pdf_file}...\n")
         reader = PdfReader(pdf_file)
         for page in reader.pages:
             personal_info += page.extract_text()
 
-    print("\nGenerating Cover-Letter...\n")
 
+    print("\nGenerating Cover-Letter...\n")
     # generate the cover letter for each job by using threading
     threads = [Thread(target=start_gpt_generation_threaded, args=(job, idx, personal_info, directory)) for idx, job in enumerate(jobs)]
     for thread in threads:
