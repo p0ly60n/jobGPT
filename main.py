cf10104f1747210d4c7475151e8142f97f3b6cd8
p0ly60n <mar_weiss@icloud.com>
2023-12-10 14:45:55 +0100

    remove docstring for main.py

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from gui import GUI
 
 if __name__ == "__main__":
-    """
-    This is the main entry point of the program.
-    It creates an instance of the GUI class and starts the application.
-    """
     app = GUI()
