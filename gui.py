85dc9348162915346e4ba63a0e9ddd1d060ad07d
p0ly60n <mar_weiss@icloud.com>
2023-12-14 14:47:03 +0100

    add scrolling functionality

--- gui.py
+++ gui.py
@@ -26,6 +26,7 @@ class App(ctk.CTk):
         self.title("Job-Scraper")
         ctk.set_default_color_theme("blue")
         ctk.set_appearance_mode("system")
+        self.resizable(False, False)
 
         self.website_choices = ["Stepstone", "Indeed"]
         self.website_choice = ctk.StringVar()
@@ -164,16 +165,21 @@ class JobWindow(ctk.CTkToplevel):
     def __init__(self, jobs: Job, output_directory: str, personal_info: str=""):
         super().__init__()
         self.title("Jobs")
+        self.grid_columnconfigure(0, minsize=1000, weight=1)
+        self.grid_rowconfigure(0, minsize=400, weight=1)
+
         self.jobs = jobs
         self.selected = []
         self.output_directory = output_directory
         self.personal_info = personal_info
 
-        self.resume_needed_label = ctk.CTkLabel(self, text="RESUME?")
-        self.title_label = ctk.CTkLabel(self, text="TITLE")
-        self.company_label = ctk.CTkLabel(self, text="COMPANY")
-        self.location_label = ctk.CTkLabel(self, text="LOCATION")
-        self.link_label = ctk.CTkLabel(self, text="LINK")
+        self.frame = ctk.CTkScrollableFrame(self)
+
+        self.resume_needed_label = ctk.CTkLabel(self.frame, text="RESUME?")
+        self.title_label = ctk.CTkLabel(self.frame, text="TITLE")
+        self.company_label = ctk.CTkLabel(self.frame, text="COMPANY")
+        self.location_label = ctk.CTkLabel(self.frame, text="LOCATION")
+        self.link_label = ctk.CTkLabel(self.frame, text="LINK")
 
         self.resume_needed_label.grid(row=0, column=0, padx=40, sticky="WE", pady=5)
         self.title_label.grid(row=0, column=1, padx=40, sticky="WE", pady=5)
@@ -186,14 +192,17 @@ class JobWindow(ctk.CTkToplevel):
         for idx, job in enumerate(self.jobs, start=0):
             var = ctk.IntVar()
             self.selected.append(var)
-            ctk.CTkCheckBox(self, variable=var, text="", width=30).grid(row=idx+1, column=0, padx=5, pady=5)
-            ctk.CTkLabel(self, text=job.job_title[0:40] + ("..." if len(job.job_title) >= 40 else "")).grid(row=idx+1, column=1, sticky="W", padx=5, pady=5)
-            ctk.CTkLabel(self, text=job.job_company[0:40] + ("..." if len(job.job_company) >= 40 else "")).grid(row=idx+1, column=2, sticky="W", padx=5, pady=5)
-            ctk.CTkLabel(self, text=job.job_location[0:40] + ("..." if len(job.job_location) >= 40 else "")).grid(row=idx+1, column=3, sticky="W", padx=5, pady=5)
-            ctk.CTkButton(self, text="click here", command=lambda link=job.job_link: self.callback(link)).grid(row=idx+1, column=4, padx=5, pady=5)
+            ctk.CTkCheckBox(self.frame, variable=var, text="", width=30).grid(row=idx+1, column=0, padx=5, pady=5)
+            ctk.CTkLabel(self.frame, text=job.job_title[0:40] + ("..." if len(job.job_title) >= 40 else "")).grid(row=idx+1, column=1, sticky="W", padx=5, pady=5)
+            ctk.CTkLabel(self.frame, text=job.job_company[0:40] + ("..." if len(job.job_company) >= 40 else "")).grid(row=idx+1, column=2, sticky="W", padx=5, pady=5)
+            ctk.CTkLabel(self.frame, text=job.job_location[0:40] + ("..." if len(job.job_location) >= 40 else "")).grid(row=idx+1, column=3, sticky="W", padx=5, pady=5)
+            ctk.CTkButton(self.frame, text="click here", command=lambda link=job.job_link: self.callback(link)).grid(row=idx+1, column=4, padx=5, pady=5)
 
-        self.generate_button = ctk.CTkButton(self, text="Generate", command=self.generate_letter, fg_color="green", hover_color="dark green")
+        self.generate_button = ctk.CTkButton(self.frame, text="Generate", command=self.generate_letter, fg_color="green", hover_color="dark green")
         self.generate_button.grid(row=len(self.jobs) + 1, column=4, padx=5, pady=5)
+
+        self.frame.grid(row=0, column=0, sticky="nsew")
+        print()
 
     def callback(self, link: str):
         """Opens a web browser with the provided link."""
